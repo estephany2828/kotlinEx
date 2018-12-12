0db30edf958b9267dfb86c3380c21d35d511bc75
estephany2828 <ejargoty@unicauca.edu.co>
2018-12-11 21:19:16 -0500

    calculadora con horizontal  y vetical

--- app/src/main/java/com/jn90/kotlinex/MainActivity.kt
+++ app/src/main/java/com/jn90/kotlinex/MainActivity.kt
@@ -7,6 +7,8 @@ import kotlinx.android.synthetic.main.activity_calculadora.*
 
 class MainActivity : AppCompatActivity() {
 
+    var value:String=""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculadora)
@@ -14,18 +16,29 @@ class MainActivity : AppCompatActivity() {
         btnSub.setOnClickListener (this::operar)
         btnMul.setOnClickListener(this::operar)
         btnDiv.setOnClickListener(this::operar)
+
+
+        value= savedInstanceState?.getString("value")?:"00"
+        rta.text = value
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString("value", value)
+        super.onSaveInstanceState(outState)
+
+    }
+
     fun operar (view: View){
         val n1 = "0$(num1.text)".toInt()
         val n2 = "0$(num2.text)".toInt()
-        val result = when (view.id){
+        value = when (view.id){
             R.id.btnAdd-> n1+n2
             R.id.btnAdd-> n1-n2
             R.id.btnAdd-> n1*n2
             else -> n1/n2
 
-        }
-
+        }.toString()
+        rta.text=value
 
     }
 }
